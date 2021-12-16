91257f7313e49fa16e5ec7a86c686f4591cf7fd3
andrew kariuki <andrew4kariuki@gmail.com>
2021-12-17 00:42:33 +0300

    improvement: add more attributes to the user entity, ensure passwords are encrypted with argon2 before saving a user.

--- packages/backend/src/entity/User.ts
+++ packages/backend/src/entity/User.ts
@@ -1,16 +1,38 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  BaseEntity,
+  UpdateDateColumn,
+  DeleteDateColumn,
+  VersionColumn,
+  BeforeInsert,
+} from "typeorm";
+
+import * as argon2 from "argon2";
 
 @Entity("users")
-export class User {
-  @PrimaryGeneratedColumn()
-  id: number;
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn("uuid") id: string;
 
-  @Column()
-  firstName: string;
+  @Column("varchar", { length: 255 })
+  name: string;
 
-  @Column()
-  lastName: string;
+  @Column("varchar", { length: 255, unique: true }) email: string;
 
-  @Column()
-  age: number;
+  @Column("text", { select: false }) password: string;
+
+  @CreateDateColumn() createdDate: Date;
+
+  @UpdateDateColumn() updatedDate: Date;
+
+  @DeleteDateColumn() deletedDate: Date;
+
+  @VersionColumn() version: number;
+
+  @BeforeInsert()
+  async hashPasswordBeforeInsert() {
+    this.password = await argon2.hash(this.password);
+  }
 }
