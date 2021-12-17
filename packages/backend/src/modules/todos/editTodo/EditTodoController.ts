dd7cfbcc68788d40f463b0cdc7e31a1e0dc84e1d
andrew kariuki <andrew4kariuki@gmail.com>
2021-12-17 17:08:14 +0300

    feat: implement edit todos controller

--- packages/backend/src/modules/todos/editTodo/EditTodoController.ts
+++ packages/backend/src/modules/todos/editTodo/EditTodoController.ts
@@ -9,12 +9,37 @@ export class EditTodoController extends BaseController {
     try {
       const { id } = req.params;
 
+      const updateData = {
+        title: req.body.title,
+        description: req.body.description,
+        complete: req.body.complete,
+      };
+
+      if (typeof updateData.title === "undefined") delete updateData.title;
+
+      if (typeof updateData.description === "undefined") delete updateData.description;
+
+      if (typeof updateData.complete === "undefined") delete updateData.complete;
+
+      console.log(updateData);
+
+      if (!updateData.title && !updateData.description && !updateData.complete) {
+        return this.clientError(res, "Can not update empty parameter.");
+      }
+
       const todo = await todoRepository.findOne({ id: parseInt(id) });
 
       const isTodo = !!todo;
       if (!isTodo) return this.clientError(res, "Todo not found");
 
-      return this.ok<any>(res, todo);
+      const update = await todoRepository.update(
+        {
+          id: parseInt(id),
+        },
+        updateData
+      );
+
+      return this.ok<any>(res, update);
     } catch (err) {
       return this.fail(res, err.toString());
     }
