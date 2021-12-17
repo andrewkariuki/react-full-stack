d6902d8e5d51086b8bda43055ae726170bf0f543
andrew kariuki <andrew4kariuki@gmail.com>
2021-12-17 16:32:56 +0300

    feat: implement get all todos by request query options - sort, limit and skip

--- packages/backend/src/modules/todos/getAllTodos/GetAllTodosController.ts
+++ packages/backend/src/modules/todos/getAllTodos/GetAllTodosController.ts
@@ -1,13 +1,33 @@
 import { Request, Response } from "express";
+import { Todo } from "../../../entity/Todo";
+import { getRepository } from "typeorm";
 import { BaseController } from "../../../controllers";
 
 export class GetAllTodosController extends BaseController {
   protected async executeImpl(req: Request, res: Response): Promise<void | any> {
-    // const todoRepository = getRepository(Todo);
+    const todoRepository = getRepository(Todo);
     try {
-      //   const { id } = req.query;
+      let todos;
+      const query = Object.keys(req.query).length > 0;
 
-      return this.ok<any>(res);
+      if (!query) {
+        todos = await todoRepository.find();
+      } else {
+        if (req.query.sort && !req.query.limit && !req.query.skip) {
+          todos = await todoRepository.find({ order: { id: (req.query as any).sort } });
+        } else if (req.query.limit && !req.query.sort && !req.query.skip) {
+          todos = await todoRepository.find({ take: (req.query as any).limit });
+        } else if (req.query.limit && req.query.skip && !req.query.sort) {
+          todos = await todoRepository.find({
+            take: (req.query as any).limit,
+            skip: (req.query as any).skip,
+          });
+        } else {
+          return this.forbidden(res, "Query does not march.");
+        }
+      }
+
+      return this.ok<any>(res, todos);
     } catch (err) {
       return this.fail(res, err.toString());
     }
