bd1fa3aa4cdadd773843441e778ad48791a7e624
andrew kariuki <andrew4kariuki@gmail.com>
2021-12-17 23:05:31 +0300

    feat: enhance routing with react router

--- packages/frontend/src/app/App.tsx
+++ packages/frontend/src/app/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 
-function App() {
-  return <div>Welcome, Home</div>;
+class App extends React.Component {
+  render() {
+    const navigation = (
+      <Routes>
+        <Route path="/" />
+      </Routes>
+    );
+    return <>{navigation}</>;
+  }
 }
 
 export default App;
