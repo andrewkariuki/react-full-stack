48977299ecce58945417b5d175fc24636f49c7d1
andrew kariuki <andrew4kariuki@gmail.com>
2021-12-18 20:36:12 +0300

    improvement: make interface improvements on charts

--- packages/frontend/src/components/chart/index.tsx
+++ packages/frontend/src/components/chart/index.tsx
@@ -1,37 +1,52 @@
 import React from "react";
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from "recharts";
+
 const data = [
   {
-    date: "Mon 10",
+    name: "Mon 10",
     uv: 4000,
+    pv: 2400,
+    amt: 2400,
   },
   {
-    date: "Tue 11",
+    name: "Tue 11",
     uv: 3000,
+    pv: 1398,
+    amt: 2210,
   },
   {
-    date: "Wed 12",
+    name: "Wed 12",
     uv: 2000,
+    pv: 9800,
+    amt: 2290,
   },
   {
-    date: "Thur 13",
+    name: "Thur 13",
     uv: 2780,
+    pv: 3908,
+    amt: 2000,
   },
   {
-    date: "Fri 14",
+    name: "Fri 14",
     uv: 1890,
+    pv: 4800,
+    amt: 2181,
   },
   {
-    date: "Sat 15",
+    name: "Start 15",
     uv: 2390,
+    pv: 3800,
+    amt: 2500,
   },
   {
-    date: "Page G",
+    name: "Sun 16",
     uv: 3490,
+    pv: 4300,
+    amt: 2100,
   },
 ];
 
-export const Chart: React.FC = () => {
+export function Chart() {
   return (
     <LineChart
       width={500}
@@ -45,11 +60,12 @@ export const Chart: React.FC = () => {
       }}
     >
       <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="date" />
+      <XAxis dataKey="name" />
       <YAxis />
       <Tooltip />
       <Legend />
       <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+      <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
     </LineChart>
   );
-};
+}
